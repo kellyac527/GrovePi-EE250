4469648945505f887c1e57fe8ba740cdf61d8942
Kelly Chan <@kellyac@usc.edu>
2020-09-01 02:07:20 -0700

    testing git

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -44,6 +44,6 @@ if __name__ == '__main__':
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
-        setRGB(0,128,64)
+        setRGB(0,128,100)
 
         print(grovepi.ultrasonicRead(PORT))
