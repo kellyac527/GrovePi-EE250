8cced9c1d02626640264a5402e20ddb56ede45e3
Kelly Chan <kellyac@usc.edu>
2020-09-01 13:06:42 -0700

    debug3.. spacing sucks2

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -59,9 +59,9 @@ if __name__ == '__main__':
 	grovepi.pinMode(potentiometer,"INPUT")
 
 	while True:
-        #So we do not poll the sensors too quickly which may introduce noise,
-        #sleep for a reasonable time of 200ms between each iteration.
-        time.sleep(0.2)
+		#So we do not poll the sensors too quickly which may introduce noise,
+		#sleep for a reasonable time of 200ms between each iteration.
+		time.sleep(0.2)
         sensor_value = grovepi.analogRead(potentiometer)
         print("sensor value ", sensor_value)
         setRGB(0,128,50)
